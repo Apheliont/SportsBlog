12decf1ecee83c11ce70b5402b0291d6075403e0
apheliont <apheliont@gmail.com>
2018-03-11 16:35:57 +0300

    pagination on index page added

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,33 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Article = require('../models/article');
 
-router.get('/', async (req, res, next) => {
-    const articles = await Article.find({}).limit(3).populate('category', {
-        title: 1
-    });
-    res.render('index', {
-        title: 'Home',
-        articles
-    });
-    next();
+router.get('/page/:page', async (req, res) => {
+    const articlesPerPage = 3;
+    const pageNumber = +req.params.page;
+    try {
+        const data = await Promise.all([
+            Article.find({})
+                .count(),
+            Article.find({})
+                .skip(articlesPerPage * pageNumber)
+                .limit(3)
+                .populate('category', {
+            title: 1
+        })]);
+
+        res.render('index', {
+            title: 'Home',
+            articles: data[1],
+            articlesNumber: data[0],
+            pageNumber
+        });
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+router.get('/', (req, res) => {
+    res.redirect("/page/0");
 });
 
 module.exports = router;
